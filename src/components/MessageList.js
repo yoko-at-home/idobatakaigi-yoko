221e5b4ccd54990edf140db2ff695ec9ef54d2a7
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-01-31 18:00:01 +0900

    fetch messages

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
+
+import { messagesRef } from '../firebase'
 
 const useStyles = makeStyles({
   root: {
@@ -8,7 +10,25 @@ const useStyles = makeStyles({
 })
 
 const MessageList = () => {
+  const [messages, setMessages] = useState([])
   const classes = useStyles()
+  useEffect(() => {
+    messagesRef
+      .orderByKey()
+      .limitToLast(3)
+      .on('value', (snapshot) => {
+        const messages = snapshot.val()
+        if (messages === null) return;
+        const entries = Object.entries(messages)
+        const newMessages = entries.map((entry) => {
+          const [key, nameAndText] = entry
+          return { key, ...nameAndText }
+        })
+        console.log(newMessages)
+        setMessages(newMessages)
+      })
+  }, [])
+
   return <div className={classes.root}>MessageList</div>
 }
 
