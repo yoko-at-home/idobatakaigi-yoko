df0459ce7b8a1a1504c749bf2adf634178f3dbb0
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-01-31 14:42:26 +0900

    remove hard coded env

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,29 @@
-import firebase from 'firebase'
-const firebaseConfig = {
-    apiKey: "AIzaSyBf-Ludry_ErYG6okJNDUKaMN37oWvuiCk",
-    authDomain: "idobatakaigi-with-yoko.firebaseapp.com",
-    projectId: "idobatakaigi-with-yoko",
-    storageBucket: "idobatakaigi-with-yoko.appspot.com",
-    messagingSenderId: "941317296283",
-    appId: "1:941317296283:web:ccd58f232de1694884363e"
-};
-firebase.initializeApp(firebaseConfig)
-const database = firebase.database();
-const messagesRef = database.ref('messages');
+import firebase from 'firebase';
 
-export const pushMessage = ({name, text}) => {
-  messagesRef.push({ name, text, });
-}
+const {
+  REACT_APP_FIREBASE_API_KEY,
+  REACT_APP_FIREBASE_AUTH_DOMAIN,
+  REACT_APP_FIREBASE_DATABASE_URL,
+  REACT_APP_FIREBASE_PROJECT_ID,
+  REACT_APP_FIREBASE_STORAGE_BUCKET,
+  REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  REACT_APP_FIREBASE_APP_ID,
+} = process.env;
+
+const firebaseConfig = {
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: REACT_APP_FIREBASE_APP_ID,
+};
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+export const messagesRef = database.ref('messages');
+
+export const pushMessage = ({ name, text }) => {
+  messagesRef.push({ name, text });
+};
